c1dcdd825da48e5494be9802efd1afef604da556
Andres Izquierdo <administracion@jmasas.com>
2025-08-08 17:11:12 -0500

    [UPD]

--- mrp_production_report_custom/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# -----------------------------------------------------------------------------
-# Módulo: Reporte personalizado de órdenes de producción
-# Autor: Luis Felipe Paternina
-# Descripción: Añade un reporte PDF consolidado por centro de trabajo a las OP.
-# -----------------------------------------------------------------------------
-
-from . import mrp_production
-from . import stock_move
